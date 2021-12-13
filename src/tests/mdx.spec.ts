4038cd35040225ebc1a7d038a0e1fd68075522e6
lbugasu <lbugasu@gmail.com>
2021-12-12 21:37:42 -0600

    tests + fix typo

---
+++ src/tests/mdx.spec.ts
@@ -0,0 +1,43 @@
+import { getFilesFromDir, getImageForPost } from "../helpers"
+
+
+const devPostData = getFilesFromDir('dev', true)
+const journalPostData = getFilesFromDir('journal', true)
+const fragmentPostData = getFilesFromDir('fragments', true)
+
+const combinedPosts = [...devPostData, ...journalPostData, ...fragmentPostData]
+
+
+combinedPosts.forEach(post => {
+    describe(`${post.title}`, () => {
+        it('title length should be greater than 3', () => {
+            expect(post.title.length).toBeGreaterThanOrEqual(3)
+        })
+
+        it('slug should not have any whitespace and URL-encoded', () => {
+            const slug = post.slug
+            const noWhitespace = !slug.match(/\s/g)
+            expect(noWhitespace).toBeTruthy
+            expect(slug).toMatch(encodeURI(slug))
+        })
+
+        it('should have a summary', () => {
+            expect(post.summary.length).toBeGreaterThanOrEqual(10)
+        })
+
+        it('post should have an image', () => {
+            const image = getImageForPost(post.slug)
+            expect(image).toBeDefined()
+        })
+
+        it('Post should have a valid date', () => {
+            const date = post.date
+            expect(date).toBeDefined()
+            expect(new Date(date)).toBeInstanceOf(Date)
+        })
+
+        it('Post should have content', () => {
+            expect(post.content.length).toBeGreaterThanOrEqual(50)
+        })
+    })
+})
