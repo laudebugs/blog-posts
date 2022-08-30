233c85f18a8ed7b4d30edcc07c5ed861290c3a30
laudebugs <lbugasu@gmail.com>
2022-08-29 21:27:55 -0400

    format

--- src/archive.ts
+++ src/archive.ts
@@ -1,37 +1,31 @@
 import fs from 'fs'
-import {getFilesFromDir} from './helpers'
+import { getFilesFromDir } from './helpers'
 
-export const writeToArchive = () =>{
-  const devPostData = getFilesFromDir('dev', false)
-  const journalPostData = getFilesFromDir('journal', false)
-  // const cuisinePostData = getFilesFromDir('cuisine', false)
-  
-  let combinedData = [...devPostData, ...journalPostData]
-  
-  combinedData = combinedData
-    .sort((a, b) => {
-      return b.date - a.date
+export const writeToArchive = () => {
+    const devPostData = getFilesFromDir('dev', false)
+    const journalPostData = getFilesFromDir('journal', false)
+    // const cuisinePostData = getFilesFromDir('cuisine', false)
+
+    let combinedData = [...devPostData, ...journalPostData]
+
+    combinedData = combinedData.sort((a, b) => {
+        return b.date - a.date
     })
-  
-  combinedData = combinedData.reverse()
-  
-  combinedData = combinedData.map((post, index) => {
-    post.no = index + 1
-    return post
-  })
-  
-  combinedData.sort((a, b) => b.no - a.no)
-  
-  fs.writeFile(
-    './out/archive.json',
-    JSON.stringify(combinedData, null, 4),
-    'utf8',
-    err => {
-      if (err) {
-        console.log('Error updating archive: ', err.message)
-      } else {
-        console.log('Successfully Updated archive✅')
-      }
-    },
-  )
+
+    combinedData = combinedData.reverse()
+
+    combinedData = combinedData.map((post, index) => {
+        post.no = index + 1
+        return post
+    })
+
+    combinedData.sort((a, b) => b.no - a.no)
+
+    fs.writeFile('./out/archive.json', JSON.stringify(combinedData, null, 4), 'utf8', (err) => {
+        if (err) {
+            console.log('Error updating archive: ', err.message)
+        } else {
+            console.log('Successfully Updated archive✅')
+        }
+    })
 }
